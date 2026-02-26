77411e6abdae880b071f4e36e20f25f4409f0f61
Wndlfa <wendel_lucas2008@hotmail.com>
2026-02-26 20:32:44 -0300

    update notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
+
 const NotFound = () => {
   const location = useLocation();
 
@@ -9,15 +10,21 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-muted">
+    <>
+    <main className="grid min-h-full place-items-center bg-[#05080F]/5 px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <p className="mb-4 text-xl text-muted-foreground">Oops! Page not found</p>
-        <a href="/" className="text-primary underline hover:text-primary/90">
-          Return to Home
-        </a>
+        <h1 className="text-4xl font-semibold text-primary">404</h1>
+        <p className="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-6xl">Oops! Página não encontrada</p>
+        <p className="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">Desculpe, não conseguimos encontrar a página que você está procurando.</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a href="/" className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
+            Retornar para a página inicial
+          </a>
+        </div>
       </div>
-    </div>
+    </main>
+
+    </>
   );
 };
 
